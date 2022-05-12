1bade976fe679aa6fad53bd048c7ef80891610f2
Aniket-Pilankar <aniketpilankar1998@gmail.com>
2022-05-13 00:36:03 +0530

    changes

--- pet-boarding-site/src/Components/Navbar/Navbar.jsx
+++ pet-boarding-site/src/Components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         <li className="nav-item">
           <Link to={'/login'} className="nav-link" >Log In</Link>
         </li>
-        {/* <li className="nav-item dropdown">
+        <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
@@ -37,7 +37,7 @@ const Navbar = () => {
         </li>
         <li className="nav-item">
           <a className="nav-link disabled">Disabled</a>
-        </li> */}
+        </li>
       </ul>
       <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
